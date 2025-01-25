5a0708853785fb51f77762c0713bed1b76a699b0
Frederik Holfeld <freddy@frederik-holfeld.net>
2025-01-25 14:06:38 +0100

    Implement rendering of individual frames
    Rendering is facilitated by the Python script from my "brpy" repository.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
 use std::{
+    env::set_current_dir,
     fs::{create_dir, read, write},
     hash::{DefaultHasher, Hash, Hasher},
     io::{ErrorKind::AlreadyExists, Read, Write},
     net::{SocketAddr, TcpListener, TcpStream},
     path::PathBuf,
-    thread,
+    process, thread,
 };
 
 #[derive(Parser)]
@@ -22,16 +23,24 @@ enum Command {
         id: String,
         blend: PathBuf,
     },
-    Render,
+    Render {
+        ip: SocketAddr,
+        output_dir: PathBuf,
+        id: String,
+        frame: usize,
+    },
     Delete,
-    Serve,
+    Serve {
+        brpy: PathBuf,
+        work_dir: PathBuf,
+    },
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(tag = "type", rename_all = "lowercase")]
 enum Request {
     Upload { id: String, size: usize },
-    Render,
+    Render { id: String, frame: usize },
     Delete,
 }
 
@@ -40,6 +49,29 @@ enum Request {
 enum Response {
     Okay,
     Fail { message: String },
+}
+
+#[derive(Serialize, Deserialize)]
+enum RenderResponse {
+    Okay { size: usize, extension: String },
+    Fail,
+}
+
+#[derive(Serialize)]
+#[serde(tag = "type", rename_all = "lowercase")]
+enum BrpyRequest {
+    Render {
+        blend: PathBuf,
+        frame: usize,
+        output: PathBuf,
+    },
+}
+
+#[derive(Deserialize)]
+#[serde(tag = "type", rename_all = "lowercase")]
+enum BrpyRenderResponse {
+    Okay { image: PathBuf },
+    Fail,
 }
 
 fn main() {
@@ -77,13 +109,52 @@ fn main() {
                 }
             }
         }
-        Command::Render => {
-            todo!();
+        Command::Render {
+            ip,
+            output_dir,
+            id,
+            frame,
+        } => {
+            set_current_dir(output_dir).unwrap();
+
+            let mut header = serde_json::to_vec(&Request::Render { id, frame }).unwrap();
+            let mut request = vec![header.len().try_into().unwrap()];
+            request.append(&mut header);
+
+            let mut server = TcpStream::connect(ip).unwrap();
+            server.write_all(&request).unwrap();
+
+            let mut len = [0; 1];
+            server.read_exact(&mut len).unwrap();
+
+            let mut header = vec![0; len[0] as usize];
+            server.read_exact(&mut header).unwrap();
+
+            let header = serde_json::from_slice(&header).unwrap();
+            match header {
+                RenderResponse::Okay { size, extension } => {
+                    let mut image = vec![0; size];
+                    server.read_exact(&mut image).unwrap();
+
+                    let image_name = format!("{:04}.{}", frame, extension);
+                    write(&image_name, image).unwrap();
+                    println!("Saved frame as {}", image_name);
+                }
+                RenderResponse::Fail => {
+                    todo!();
+                }
+            }
         }
         Command::Delete => {
             todo!();
         }
-        Command::Serve => {
+        Command::Serve { brpy, work_dir } => {
+            if !brpy.is_file() {
+                panic!("BRPy script {} either does not exist, access is not permitted or it's not a file", brpy.display());
+            }
+
+            set_current_dir(work_dir).unwrap();
+
             if let Err(error) = create_dir("anonymous") {
                 match error.kind() {
                     AlreadyExists => {}
@@ -106,8 +177,10 @@ fn main() {
             for stream in listener.incoming() {
                 match stream {
                     Ok(stream) => {
+                        let brpy = brpy.clone();
+
                         thread::spawn(move || {
-                            handle_client(stream);
+                            handle_client(stream, brpy);
                         });
                     }
                     Err(error) => {
@@ -119,7 +192,7 @@ fn main() {
     }
 }
 
-fn handle_client(mut client: TcpStream) {
+fn handle_client(mut client: TcpStream, brpy: PathBuf) {
     loop {
         let mut len = [0; 1];
         client.read_exact(&mut len).unwrap();
@@ -158,8 +231,81 @@ fn handle_client(mut client: TcpStream) {
                 println!("Saved .blend file with ID \"{}\"", id);
                 break;
             }
-            Request::Render => {
-                todo!();
+            Request::Render { id, frame } => {
+                let mut hasher = DefaultHasher::new();
+                id.hash(&mut hasher);
+                let hash = hasher.finish();
+
+                if let Err(error) = create_dir(format!("anonymous/{}/render", hash)) {
+                    match error.kind() {
+                        AlreadyExists => {}
+                        _ => {
+                            panic!("{}", error);
+                        }
+                    }
+                }
+
+                let listener = TcpListener::bind("localhost:0").unwrap();
+                let port = listener.local_addr().unwrap().port();
+
+                let mut process = process::Command::new("blender")
+                    .args([
+                        "--background",
+                        "--python",
+                        brpy.to_str().unwrap(),
+                        "--",
+                        &port.to_string(),
+                    ])
+                    .spawn()
+                    .unwrap();
+
+                let (mut brpy, _) = listener.accept().unwrap();
+
+                let mut header = serde_json::to_vec(&BrpyRequest::Render {
+                    blend: format!("anonymous/{0}/{0}.blend", hash).into(),
+                    frame,
+                    output: format!("anonymous/{}/render", hash).into(),
+                })
+                .unwrap();
+
+                let mut request = vec![header.len().try_into().unwrap()];
+                request.append(&mut header);
+
+                brpy.write_all(&request).unwrap();
+
+                let mut len = [0; 1];
+                brpy.read_exact(&mut len).unwrap();
+
+                let mut header = vec![0; len[0] as usize];
+                brpy.read_exact(&mut header).unwrap();
+
+                let header = serde_json::from_slice(&header).unwrap();
+                match header {
+                    BrpyRenderResponse::Okay { image } => {
+                        let extension = String::from(image.extension().unwrap().to_str().unwrap());
+                        let mut image = read(image).unwrap();
+
+                        let mut header = serde_json::to_vec(&RenderResponse::Okay {
+                            size: image.len(),
+                            extension,
+                        })
+                        .unwrap();
+
+                        let mut response = vec![header.len().try_into().unwrap()];
+                        response.append(&mut header);
+                        response.append(&mut image);
+
+                        client.write_all(&response).unwrap();
+                    }
+                    BrpyRenderResponse::Fail => {
+                        todo!();
+                    }
+                }
+
+                let _ = process.wait();
+
+                println!("Rendered frame {} of \"{}\" sent to client", frame, id);
+                break;
             }
             Request::Delete => {
                 todo!();
